2ea2ce1c9839bf33e7d5087f3c06daa3ef16cc56
Howard Wilson <howbotnik@me.com>
2019-12-14 22:58:52 +0000

    adapted state.txt file reading for linking to a relative path

--- miningState.py
+++ miningState.py
@@ -1,15 +1,17 @@
 import logging
+import pathlib
 
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
+fn = pathlib.Path(__file__).parent / 'state.txt'
 
 def getState():
-    file = open("D:\\Documents\\Mining\\sunmine\\state.txt", "r")
+    file = open(fn, "r")
     readLine = file.readline()
     file.close()
     return readLine
 
 def setState(state):
     logging.debug("Editing file.")
-    file = open("D:\\Documents\\Mining\\sunmine\\state.txt", "w")
+    file = open(fn, "w")
     file.write(state)
     file.close()
