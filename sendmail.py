7108e7777af1d2524f52840410fdf2460cbae752
Howard Wilson <howbotnik@me.com>
2019-12-17 13:54:22 +0000

    Improved message in email

--- sendmail.py
+++ sendmail.py
@@ -4,7 +4,7 @@ import logging
 
 def send(message):
     logging.debug("Sending email.")
-    message = 'Subject: {}\n\n{}'.format(createSubject(), createEmailBody(message))
+    message = 'Subject: {}\n\n{}'.format(createSubject(message), createEmailBody(message))
     server = smtplib.SMTP(config.getSMTPServer(), 25)
     server.connect(config.getSMTPServer(), config.getSMTPPort())
     server.ehlo()
@@ -14,8 +14,8 @@ def send(message):
     server.sendmail(config.getSenderEmail(), config.getRecipientEmail(), message)
     server.quit()
 
-def createSubject():
-    return "SUNMINE: "
+def createSubject(message):
+    return "SUNMINE: " + message
 
 def createEmailBody(message):
-    return "Sunmine has set your mining rig to..." + message
+    return "Sunmine has set your mining rig to: " + message
