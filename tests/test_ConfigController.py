79db1bce7c811bad5d30c3578240152e6eab4fad
Howard <howbotnik@me.com>
2020-01-25 08:21:32 +0000

    adjusted a few imports in test class

--- tests/test_ConfigController.py
+++ tests/test_ConfigController.py
@@ -1,47 +1,60 @@
 from unittest import TestCase
 from ConfigController import ConfigController
 
+
 class TestConfigController(TestCase):
-    config = ConfigController()
 
     def test_get_country_code(self):
+        config = ConfigController()
         # check to see if returns type str
-        self.assertIsInstance(self.config.get_country_code(), str)
+        self.assertIsInstance(config.get_country_code(), str)
         # check to see if returned value is 2 char long
-        self.assertEqual(len(self.config.get_country_code()), 2)
+        self.assertEqual(len(config.get_country_code()), 2)
 
     def test_get_location(self):
-        self.assertIsInstance(self.config.get_location(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_location(), str)
 
     def test_get_latitude(self):
-        self.assertIsInstance(self.config.get_latitude(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_latitude(), str)
 
     def test_get_longitude(self):
-        self.assertIsInstance(self.config.get_longitude(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_longitude(), str)
 
     def test_get_smtp_server(self):
-        self.assertIsInstance(self.config.get_smtp_server(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_smtp_server(), str)
 
     def test_get_smtp_port(self):
-        self.assertIsInstance(self.config.get_smtp_port(), int)
+        config = ConfigController()
+        self.assertIsInstance(config.get_smtp_port(), int)
 
     def test_get_sender_email(self):
-        self.assertIsInstance(self.config.get_sender_email(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_sender_email(), str)
 
     def test_get_password(self):
-        self.assertIsInstance(self.config.get_password(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_password(), str)
 
     def test_get_recipient_email(self):
-        self.assertIsInstance(self.config.get_recipient_email(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_recipient_email(), str)
 
     def test_get_open_weather_token(self):
-        self.assertIsInstance(self.config.get_open_weather_token(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_open_weather_token(), str)
 
     def test_get_program_location(self):
-        self.assertIsInstance(self.config.get_program_location(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_program_location(), str)
 
     def test_get_program_name(self):
-        self.assertIsInstance(self.config.get_program_name(), str)
+        config = ConfigController()
+        self.assertIsInstance(config.get_program_name(), str)
 
     def test_get_weather_codes(self):
-        self.assertIsInstance(self.config.get_weather_codes(), list)
+        config = ConfigController()
+        self.assertIsInstance(config.get_weather_codes(), list)
